a81971d0dc3efd9f1f6d5d545d40e346e2248b33
Gabriel Brammer <gbrammer@gmail.com>
2023-02-01 16:51:37 +0100

    pull out msa helpers

--- msaexp/tests/test_utils.py
+++ msaexp/tests/test_utils.py
@@ -18,12 +18,3 @@ def test_wavelength_grids():
         grid = utils.get_standard_wavelength_grid(gr, log_step=True)
         
     grid = utils.get_standard_wavelength_grid('prism', free_prism=False)
-
-
-def test_meta_parser():
-    
-    uri = 'https://mast.stsci.edu/api/v0.1/Download/file?uri=mast:JWST/product/'
-    regs = utils.regions_from_metafile(uri+'jw02756001001_01_msa.fits',
-                                       as_string=True,
-                                       with_bars=True)
-    
