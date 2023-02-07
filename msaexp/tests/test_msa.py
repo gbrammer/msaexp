12e0af4afe466c362575107285eabebe1ee6437c
Gabriel Brammer <gbrammer@gmail.com>
2023-02-07 14:49:12 +0100

    add more tests

--- msaexp/tests/test_msa.py
+++ msaexp/tests/test_msa.py
@@ -1,6 +1,20 @@
 from .. import msa
 
 def test_meta_parser():
+    import matplotlib.pyplot as plt
+    
+    uri = 'https://mast.stsci.edu/api/v0.1/Download/file?uri=mast:JWST/product/'
+    meta = msa.MSAMetafile(uri+'jw02756001001_01_msa.fits')
+    
+    regs = meta.regions_from_metafile(as_string=True, with_bars=True)
+    
+    fig, ax = plt.subplots(1,1,figsize=(8,8))
+    _ = meta.plot_slitlet(source_id=110003, cutout_size=12.5,
+                          rgb_filters=None, ax=ax)
+    plt.close('all')
+
+
+def test_wrapper():
     
     uri = 'https://mast.stsci.edu/api/v0.1/Download/file?uri=mast:JWST/product/'
     regs = msa.regions_from_metafile(uri+'jw02756001001_01_msa.fits',
