9d342cd85f2dae2435f14c5201733a5e98d8d9a5
Gabriel Brammer <gbrammer@gmail.com>
2023-02-17 10:18:20 +0100

    fix units of dispersion tables

---
+++ msaexp/data/fix_units.py
@@ -0,0 +1,19 @@
+"""
+Fix units of the dispersion tables to be astropy compatible
+"""
+
+import astropy.units as u
+from astropy.table import Table
+
+import glob
+
+files = glob.glob('*disp.fits')
+files.sort()
+
+for file in files:
+    tab = Table.read(file)
+    tab['WAVELENGTH'].unit = u.micron
+    tab['DLDS'].unit = u.micron/u.pixel
+    tab['R'].unit = None
+    tab.write(file, overwrite=True)
+    
