correct flam units in figure

msaexp/utils.py
@@ -1831,9 +1831,9 @@ def drizzled_hdu_figure(hdul, tick_steps=None, xlim=None, subplot_args=dict(figs
         ymax = np.nanpercentile(flux[err > 0], 90)*ymax_sigma_scale
         ymax = np.maximum(ymax, 7*np.median(err[err > 0]))
     
-    yscl = hdul['PROFILE'].data.max()/ap_corr
+    yscl = hdul['PROFILE'].data.max()*ap_corr
     if unit == 'flam':
-        yscl = yscl*to_flam #(sp['wave']/2.)**2
+        yscl = yscl/to_flam #(sp['wave']/2.)**2
     
     if smooth_sigma is not None:
         xp = np.arange(-4*int(smooth_sigma), 5*int(smooth_sigma))