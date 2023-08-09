fec0cf3a534175dc048768dc7c59578f77a14b3f
Gabriel Brammer <gbrammer@gmail.com>
2023-08-09 11:21:41 +0200

    add PAH template

--- msaexp/spectrum.py
+++ msaexp/spectrum.py
@@ -20,6 +20,21 @@ import astropy.io.fits as pyfits
 from grizli import utils
 utils.set_warnings()
 
+try:
+    import eazy
+    wave = np.exp(np.arange(np.log(2.4), np.log(4.5), 1./4000))*1.e4
+    _temp = utils.pah33(wave)
+    PAH_TEMPLATES = {}
+    for t in _temp:
+        if '3.47' in t:
+            continue
+            
+        _tp = _temp[t]
+        PAH_TEMPLATES[t] = eazy.templates.Template(name=t, arrays=(_tp.wave, _tp.flux))
+except:
+    print('Failed to initialize PAH_TEMPLATES')
+    PAH_TEMPLATES = {}
+
 import grizli.utils_c
 import astropy.units as u
 
@@ -615,7 +630,7 @@ H_RECOMBINATION_LINES = ['Ha+NII', 'Ha','Hb','Hg','Hd',
                          'PaA','PaB','PaG','PaD','Pa8',
                          'BrA','BrB','BrG','BrD']
 
-def make_templates(sampler, z, bspl={}, eazy_templates=None, vel_width=100, broad_width=4000, broad_lines=[], scale_disp=1.3, use_full_dispersion=False, disp=None, grating='prism', halpha_prism=['Ha+NII'], oiii=['OIII'], o4363=[], sii=['SII'], lorentz=False, **kwargs):
+def make_templates(sampler, z, bspl={}, eazy_templates=None, vel_width=100, broad_width=4000, broad_lines=[], scale_disp=1.3, use_full_dispersion=False, disp=None, grating='prism', halpha_prism=['Ha+NII'], oiii=['OIII'], o4363=[], sii=['SII'], lorentz=False, with_pah=True, **kwargs):
     """
     Generate fitting templates
     
@@ -777,7 +792,24 @@ def make_templates(sampler, z, bspl={}, eazy_templates=None, vel_width=100, broa
             _A.append(line_0/1.e4)
             templates.append(name)
             tline.append(True)
-
+        
+        if with_pah:
+            xpah = 3.3*(1+z)
+            if ((xpah > wmin) & (xpah < wmax)) | (0):
+                for t in PAH_TEMPLATES:
+                    tp = PAH_TEMPLATES[t]
+                    tflam = sampler.resample_eazy_template(tp,
+                                            z=z,
+                                            velocity_sigma=vel_width,
+                                            scale_disp=scale_disp,
+                                            fnu=False)
+            
+                    _A.append(tflam)
+            
+                    templates.append(t)
+                    tline.append(True)
+                    
+                
         _A = np.vstack(_A)
         
         ll = wobs.value*1.e4/(1+z) < 1215.6
