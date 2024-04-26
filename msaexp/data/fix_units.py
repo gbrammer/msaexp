50c2a1b8db001243972b9ecc695197833e2153f6
Gabriel Brammer <gbrammer@gmail.com>
2024-04-27 00:27:08 +0200

    reformat fix_units

--- msaexp/data/fix_units.py
+++ msaexp/data/fix_units.py
@@ -7,13 +7,12 @@ from astropy.table import Table
 
 import glob
 
-files = glob.glob('*disp.fits')
+files = glob.glob("*disp.fits")
 files.sort()
 
 for file in files:
     tab = Table.read(file)
-    tab['WAVELENGTH'].unit = u.micron
-    tab['DLDS'].unit = u.micron/u.pixel
-    tab['R'].unit = None
+    tab["WAVELENGTH"].unit = u.micron
+    tab["DLDS"].unit = u.micron / u.pixel
+    tab["R"].unit = None
     tab.write(file, overwrite=True)
-    
