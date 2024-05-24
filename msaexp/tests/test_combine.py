980d363ad7e6a66d042dfba9260df6d7723541dd
Gabriel Brammer <gbrammer@gmail.com>
2024-05-24 15:15:53 +0200

    fix test

--- msaexp/tests/test_combine.py
+++ msaexp/tests/test_combine.py
@@ -113,6 +113,7 @@ def test_slit_group():
         bar_corr_mode="wave",
         fix_prism_norm=True,
         sky_arrays=None,
+        sky_file=None,
         estimate_sky_kwargs=None,
         flag_profile_kwargs={},
         flag_percentile_kwargs={},
@@ -219,7 +220,7 @@ def test_slit_group():
         "position_key": "y_index",
         "nhot": 0,
         "ncold": 0,
-        # "has_sky_arrays": False,
+        "has_sky_arrays": False,
         "weight_type": "ivm",
         "percentile_outliers": 0,
         "removed_pathloss": "PATHLOSS_UN",
@@ -233,7 +234,8 @@ def test_slit_group():
     assert np.allclose(np.nanmax(obj.wave), 5.3821, rtol=1.0e-3)
 
     assert np.nanmin(obj.bar[obj.mask]) > default_kwargs["min_bar"]
-
+    
+    # These are the same if there is no sky model
     assert np.allclose(obj.data[obj.mask], obj.sci[obj.mask])
 
     assert np.allclose(np.nanmin(obj.fixed_yshutter), -1.13236, rtol=1.0e4)
@@ -307,7 +309,11 @@ def test_slit_group():
         absolute_threshold=0.2,
         make_plot=False,
     )
-
+    
+    # Read global sky
+    obj.get_global_sky(sky_file="read")
+    assert obj.meta["sky_file"] == "jw01345062001_sky.csv"
+    
     obj.apply_normalization_correction()
 
     obj.meta["bar_corr_mode"] = "slit"
