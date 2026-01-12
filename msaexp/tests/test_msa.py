56bfd79ab430253ad5aa3b6089ff297d986b792e
Gabriel Brammer <gbrammer@gmail.com>
2026-01-12 17:00:24 +0100

    add test against G395M

--- msaexp/tests/test_msa.py
+++ msaexp/tests/test_msa.py
@@ -161,7 +161,7 @@ def test_pad():
 
 
 def test_wavelength_limits():
-    
+    import matplotlib.pyplot as plt
     import grizli.utils
     
     cols, rows, quadrants = [], [], []
@@ -193,46 +193,59 @@ def test_wavelength_limits():
             assert(np.allclose(tab[c][v1 & v2], ref[c][v1 & v2], rtol=1.e-2))
 
     # Compare to "MSA Target Info" table export from APT
-    apt = grizli.utils.read_catalog(
-        os.path.join(
-            os.path.dirname(__file__),
-            "data",
-            "4233-obs2-exp1-c1_uds_obs2az_prism_bkge1n1-PRISM-CLEAR.csv"
+    MAKE_PLOT = False
+
+    for APT_FILE in [
+        "4233-obs2-exp1-c1_uds_obs2az_prism_bkge1n1-PRISM-CLEAR.csv",
+        "4233-obs2-exp2-c1_uds_obs2az_g395me2n1-G395M-F290LP.csv",
+    ]:
+        apt = grizli.utils.read_catalog(
+            os.path.join(
+                os.path.dirname(__file__),
+                "data",
+                APT_FILE
+            )
         )
-    )
 
-    tab = msa.get_shutter_wavelength_limits(
-        apt['Column (Disp)'],
-        apt['Row (Spat)'],
-        apt['Quadrant'],
-        grating='prism', filter='clear'
-    )
+        grating = os.path.basename(APT_FILE).split('-')[-2].lower()
+        filter = os.path.basename(APT_FILE).split('-')[-1].lower().split('.')[0]
 
-    for det in ['NRS1','NRS2']:
-        for limit in ['Min', 'Max']:
-            this_col = f'{det}_wave_{limit}'.lower()
-            apt_col = f'{det} {limit} Wave'
-            # plt.scatter(
-            #     apt[apt_col], tab[this_col],
-            #     c=tab['quadrant'],
-            #     alpha=0.5
-            # )
-            # apt_ll = apt[apt_col] == -2.0
-            # tab[apt_col] = apt[apt_col]
+        tab = msa.get_shutter_wavelength_limits(
+            apt['Column (Disp)'],
+            apt['Row (Spat)'],
+            apt['Quadrant'],
+            grating=grating, filter=filter,
+        )
 
-            test = np.isfinite(tab[this_col] + apt[apt_col])
-            test &= apt[apt_col] > 0
+        if MAKE_PLOT:
+            fig, ax = plt.subplots(1,1)
 
-            if test.sum() > 0:
-                assert np.allclose(
-                    tab[this_col][test],
-                    apt[apt_col][test],
-                    atol=0.15
-                )
-
-                #
+        for det in ['NRS1','NRS2']:
+            for limit in ['Min', 'Max']:
+                this_col = f'{det}_wave_{limit}'.lower()
+                apt_col = f'{det} {limit} Wave'
                 # plt.scatter(
-                #     apt[apt_col][test],
-                #     (tab[this_col] - apt[apt_col])[test],
-                #     c=tab['quadrant'][test],
+                #     apt[apt_col], tab[this_col],
+                #     c=tab['quadrant'],
+                #     alpha=0.5
                 # )
+                # apt_ll = apt[apt_col] == -2.0
+                # tab[apt_col] = apt[apt_col]
+
+                test = np.isfinite(tab[this_col] + apt[apt_col])
+                test &= apt[apt_col] > 0
+
+                if test.sum() > 0:
+                    if MAKE_PLOT:
+                        plt.scatter(
+                            apt[apt_col][test],
+                            (tab[this_col] - apt[apt_col])[test],
+                            c=tab['quadrant'][test],
+                        )
+                    else:
+                        assert np.allclose(
+                            tab[this_col][test],
+                            apt[apt_col][test],
+                            atol=0.15
+                        )
+
