add badpix mask and fix bug in nod diff variance

msaexp/cloud/lrs.py
@@ -32,6 +32,16 @@ CRDS_CONTEXT = "jwst_1464.pmap"
 
 LOGGER = logging.getLogger(__name__)
 
+def load_lrs_badpix():
+    """
+    Load ``msaexp/data/lrs/miri_lrs_badpix.txt table``
+    """
+    badpix_file = os.path.join(
+        msautils.module_data_path(), "lrs", "miri_lrs_badpix.txt"
+    )
+    lrs_bpix = utils.read_catalog(badpix_file)
+    return lrs_bpix
+
 def full_lrs_pipeline(
     prog=5141,
     obs=1,
@@ -46,6 +56,7 @@ def full_lrs_pipeline(
     run_level2=True,
     skip_photom_step=True,
     do_nod_subtraction=True,
+    flatten_bkg_trend=False,
     optimize_trace_niter=3,
     x0=[0.0, 0.0],
     nod_ext="cal",
@@ -195,7 +206,11 @@ def full_lrs_pipeline(
 
     if do_nod_subtraction:
         s2 = nod_sky_subtraction(
-            dir="Level2", file_query=file_query, ext=nod_ext, bkg_threshold=0.3
+            dir="Level2",
+            file_query=file_query,
+            ext=nod_ext,
+            bkg_threshold=0.3,
+            flatten_bkg_trend=flatten_bkg_trend,
         )
 
     result = {
@@ -911,7 +926,7 @@ def lrs_regions(res, output="lrs.reg"):
     res["illum_footprint"] = illum_polygons
 
 
-def nod_sky_subtraction(dir="Level2", file_query="jw0", ext="cal", bkg_threshold=0.3):
+def nod_sky_subtraction(dir="Level2", file_query="jw0", ext="cal", bkg_threshold=0.3, flatten_bkg_trend=True, **kwargs):
     """
     Compute differences of nodded exposures for sky subtraction
     """
@@ -919,6 +934,8 @@ def nod_sky_subtraction(dir="Level2", file_query="jw0", ext="cal", bkg_threshold
     full_file_query = os.path.join(dir, file_query + "*" + ext + ".fits")
 
     s2_files = glob.glob(full_file_query)
+
+    lrs_bpix = load_lrs_badpix()
 
     if (ext == "bkg") & (len(s2_files) == 0):
         ext = "cal"
@@ -944,6 +961,10 @@ def nod_sky_subtraction(dir="Level2", file_query="jw0", ext="cal", bkg_threshold
             dm.close()
             continue
 
+        # Apply badpix mask
+        dm.dq[lrs_bpix["yi"], lrs_bpix["xi"]] |= 1
+        dq_mask = (dm.dq & 1) > 0
+
         # targ, grp, dith, _, _ = file.split('_')
 
         dith = f"{dm.meta.dither.x_offset:>4.1f}"
@@ -951,10 +972,25 @@ def nod_sky_subtraction(dir="Level2", file_query="jw0", ext="cal", bkg_threshold
 
         if targ not in s2:
             s2[targ] = {}
+
         if dith not in s2[targ]:
-            s2[targ][dith] = {"dm": []}
+            s2[targ][dith] = {"dm": [], "scl": []}
+
+        if flatten_bkg_trend:
+            if (i == 0):
+                bkg_ref = dm.data * np.nan**dq_mask
+                bkg_ratio_i = 1.0
+            else:
+                bkg_ratio_i = np.nanmedian(dm.data / bkg_ref)
+
+            msg = f"flatten_bkg_trend: {file} {bkg_ratio_i:8.3f}"
+            utils.log_comment(utils.LOGFILE, msg, verbose=True)
+
+        else:
+            bkg_ratio_i = 1.0
 
         s2[targ][dith]["dm"].append(dm)
+        s2[targ][dith]["scl"].append(bkg_ratio_i)
 
     if ext == "bkg":
         return s2
@@ -968,12 +1004,22 @@ def nod_sky_subtraction(dir="Level2", file_query="jw0", ext="cal", bkg_threshold
             msg = f'  dither {dith}: {len(s2[targ][dith]["dm"])} exposures'
             utils.log_comment(utils.LOGFILE, msg, verbose=True)
 
-            s2[targ][dith]["avg"] = np.nanmean(
-                np.array([dm.data for dm in s2[targ][dith]["dm"]]), axis=0
+            s2_i = s2[targ][dith]
+
+            s2_i["avg"] = np.nanmean(
+                np.array([
+                    dm.data / scl * np.nan**(dm.dq & 1)
+                    for (dm, scl) in zip(s2_i["dm"], s2_i["scl"])
+                ]),
+                axis=0
             )
 
-            s2[targ][dith]["var"] = np.nanmean(
-                np.array([dm.err**2 for dm in s2[targ][dith]["dm"]]), axis=0
+            s2_i["var"] = np.nanmean(
+                np.array([
+                    (dm.err / scl * np.nan**(dm.dq & 1))**2
+                    for (dm, scl) in zip(s2_i["dm"], s2_i["scl"])
+                ]),
+                axis=0
             )
 
         for d1 in s2[targ]:
@@ -995,11 +1041,14 @@ def nod_sky_subtraction(dir="Level2", file_query="jw0", ext="cal", bkg_threshold
                 other_avg = np.nanmean(np.array(other_dithers), axis=0)
                 other_var = np.nanmean(np.array(other_vars), axis=0)
 
-            for dm in s2[targ][d1]["dm"]:
-                dm.data -= other_avg
-                dm.err += np.sqrt(other_var)
+            s2_i = s2[targ][d1]
+
+            for dm, scl in zip(s2_i["dm"], s2_i["scl"]):
+                dm.data = (dm.data / scl - other_avg) * scl
+                dm.err = np.sqrt((dm.err / scl)**2 + other_var) * scl
 
                 dm.meta.filename = dm.meta.filename.replace("_bkg", "")
+
                 out_file = os.path.join(
                     dir, dm.meta.filename.replace(ext, ext + "_bkg")
                 )

msaexp/ifu.py
@@ -448,7 +448,7 @@ def saturated_mask(files, ptab):
         print(f"{file}  {ovalid} > {nvalid}  ({ovalid - nvalid})")
 
 
-def plot_cube_strips(ptabs, figsize=(10, 5), cmap="bone_r", **kwargs):
+def plot_cube_strips(ptabs, figsize=(10, 5), cmap="bone_r", alpha=0.05, vmin=-0.1, vmax=5.6, **kwargs):
     """
     Plot spatial slices of a cube pixel table
     """
@@ -477,7 +477,7 @@ def plot_cube_strips(ptabs, figsize=(10, 5), cmap="bone_r", **kwargs):
             ixi = un[v] & (
                 np.abs(ptab["wave"] - np.nanmedian(ptab["wave"])) < 0.01
             )
-            ax.scatter(dx[ixi], dy[ixi], alpha=0.1, color=color)
+            ax.scatter(dx[ixi], dy[ixi], alpha=alpha, color=color)
 
         # Cube data
         ax = axes[1]
@@ -507,10 +507,10 @@ def plot_cube_strips(ptabs, figsize=(10, 5), cmap="bone_r", **kwargs):
             dy[test] + ry,
             c=ptab["data"][test],
             cmap=cmap,
-            alpha=0.3,
+            alpha=alpha,
             marker="s",
-            vmin=-0.1,
-            vmax=5.6,
+            vmin=vmin,
+            vmax=vmax,
             s=30,
             ec="None",
         )
@@ -1216,7 +1216,7 @@ def slice_corners(
             transform = slice_wcs.get_transform("detector", "slit_frame")
 
             slits = input.meta.wcs.get_transform("gwa", "slit_frame").slits
-            print("xxx", slits, slits[i])
+            # print("xxx", slits, slits[i])
 
             slice_wcs.bounding_box = nirspec.compute_bounding_box(
                 transform, slits[i], slice_wavelength_range, refine=True
@@ -1626,7 +1626,9 @@ def ifu_pipeline(
     perform_saturation=False,
     cumulative_saturation=True,
     exclude_self_saturation=True,
+    center_coords=None,
     recenter_cube=False,
+    show_strips=True,
     **kwargs,
 ):
     """
@@ -1644,10 +1646,14 @@ def ifu_pipeline(
         Rootname of output file
 
     use_first_center : bool
-        Flag to use the reference coordinate as the first exposure for all exposures
+        Flag to use the reference coordinate as the first exposure for all
+        exposures
 
     make_drizzled : bool
         Make rectified resampled cube
+
+    show_strips : bool
+        Make strip figure diagnostic with `~msaexp.ifu.plot_cube_strips`
 
     kwargs : dict
         Keyword arguments passed to all sub functions
@@ -1679,19 +1685,24 @@ def ifu_pipeline(
         files = [os.path.basename(file) for file in res["dataURI"]]
         files.sort()
     else:
-        obsid = files[0][2:13]
+        if obsid is None:
+            obsid = os.path.basename(files[0])[2:13]
+
         if grating is None:
             with pyfits.open(files[0]) as im:
                 grating = im[0].header["GRATING"]
 
     msg = f"ifu_pipeline: file={files[0]}  {obsid} {grating}"
+    #print("xxx", msg, f"cube-{obsid}-{grating}{output_suffix}.log.txt".lower())
+
     utils.LOGFILE = f"cube-{obsid}-{grating}{output_suffix}.log.txt".lower()
     utils.log_comment(utils.LOGFILE, msg, verbose=VERBOSITY)
 
     # Initialize
     cubes = []
     for file in files:
-        cube = ExposureCube(file, **kwargs)
+        file_ = os.path.basename(file) if file.startswith("s3://") else file
+        cube = ExposureCube(file_, **kwargs)
         cubes.append(cube)
 
         if cube.ptab is None:
@@ -1733,9 +1744,12 @@ def ifu_pipeline(
     # Saturated mask
     sat_files, sat_data = [], []
     for file in files:
+        file_ = os.path.basename(file) if file.startswith("s3://") else file
+
         sat_file, sat_i = msautils.exposure_ramp_saturation(
-            file, perform=perform_saturation, **kwargs
+            file_, perform=perform_saturation, **kwargs
         )
+
         if sat_i is None:
             sat_i = np.zeros((2048, 2048), dtype=bool)
 
@@ -1754,8 +1768,6 @@ def ifu_pipeline(
         ptab_sat = sat_mask[cube.ptab["ypix"], cube.ptab["xpix"]]
         if exclude_self_saturation:
             ptab_sat ^= this_sat[cube.ptab["ypix"], cube.ptab["xpix"]]
-
-        # print("xxx", files[i], ptab_sat.sum())
 
         cube.ptab["dq"] |= (ptab_sat * 4096).astype(cube.ptab["dq"].dtype)
         if cumulative_saturation:
@@ -1779,11 +1791,23 @@ def ifu_pipeline(
     ptab = astropy.table.vstack(ptabs)
     ptab.meta["srcname"] = SOURCE
     ptab.meta["obsid"] = obsid
+
     if "proposer_ra" in ptabs[0].meta:
         ptab.meta["ra_ref"] = ptabs[0].meta["proposer_ra"]
         ptab.meta["dec_ref"] = ptabs[0].meta["proposer_dec"]
 
-    if recenter_cube:
+    if center_coords is not None:
+
+        oref = ptab.meta["ra_ref"] * 1, ptab.meta["dec_ref"] * 1
+
+        ptab.meta["ra_ref"] = center_coords[0]
+        ptab.meta["dec_ref"] = center_coords[1]
+
+        msg = f"User-defined center: ({oref[0]:>.6f}, {oref[1]:.6f}) -> "
+        msg += f'({ptab.meta["ra_ref"]:>.6f}, {ptab.meta["dec_ref"] :.6f})'
+        utils.log_comment(utils.LOGFILE, msg, verbose=VERBOSITY)
+
+    elif recenter_cube:
         oref = ptab.meta["ra_ref"] * 1, ptab.meta["dec_ref"] * 1
 
         coo_ = np.array([ptab["ra"], ptab["dec"]])
@@ -1799,23 +1823,24 @@ def ifu_pipeline(
         msg += f'({ptab.meta["ra_ref"]:>.6f}, {ptab.meta["dec_ref"] :.6f})'
         utils.log_comment(utils.LOGFILE, msg, verbose=VERBOSITY)
 
-    strip_fig = plot_cube_strips(
-        ptabs, ref_coords=(ptab.meta["ra_ref"], ptab.meta["dec_ref"])
-    )
+    if show_strips:
+        strip_fig = plot_cube_strips(
+            ptabs, ref_coords=(ptab.meta["ra_ref"], ptab.meta["dec_ref"])
+        )
 
-    strip_fig.text(
-        0.5,
-        0.005,
-        outroot,
-        ha="center",
-        va="bottom",
-        transform=strip_fig.transFigure,
-        fontsize=8,
-    )
+        strip_fig.text(
+            0.5,
+            0.005,
+            outroot,
+            ha="center",
+            va="bottom",
+            transform=strip_fig.transFigure,
+            fontsize=8,
+        )
 
-    utils.figure_timestamp(strip_fig)
+        utils.figure_timestamp(strip_fig)
 
-    strip_fig.savefig(f"{outroot}.strips.png")
+        strip_fig.savefig(f"{outroot}.strips.png")
 
     ptab.meta["nfiles"] = len(files)
     for i, file_ in enumerate(files):
@@ -2008,17 +2033,24 @@ def pixel_table_to_detector(
         return detector_array
 
 
-def pixel_table_dx_dy(ptab, ref_coords=None, wcs=None, **kwargs):
+def pixel_table_dx_dy(ptab, ref_coords=None, pa="meta", wcs=None, **kwargs):
     """
     Parameters
     ----------
     ptab : Table
-        IFU cube pixel table with (at least) columns ``ra``, ``dec`` and attribute
-        ``meta["position_angle"]``
+        IFU cube pixel table with (at least) columns ``ra``, ``dec``
 
     ref_coords : (float, float), None
         Reference decimal ra, dec.  If not specified, then get from ``ra_ref``, ``dec_ref``
         entries in the metadata
+
+    pa : float, str
+        Position angle, degrees from North towards East.  If ``pa="meta"``,
+        get from ``ptab.meta["position_angle"]``
+
+    wcs : `~astropy.wcs.WCS`, None
+        If provided, return the pixel indices of ``wcs`` transformed from
+        the sky coordinates in ``ptab``.
 
     Returns
     -------
@@ -2030,7 +2062,10 @@ def pixel_table_dx_dy(ptab, ref_coords=None, wcs=None, **kwargs):
         dx, dy = wcs.all_world2pix(ptab["ra"], ptab["dec"], 0)
         return dx, dy
 
-    rot = rotation_matrix(ptab.meta["position_angle"])
+    if pa in ["meta"]:
+        pa = ptab.meta["position_angle"]
+
+    rot = rotation_matrix(pa)
 
     if ref_coords is None:
         ra_ref = ptab.meta["ra_ref"]
@@ -2822,6 +2857,8 @@ class ReducedCube:
                     self.sci = cube_hdu["DATA"].data * 1
                     self.wht = 1.0 / cube_hdu["STAT"].data
 
+                    self.primary_header = cube_hdu[0].header.copy()
+                    
                     self.header["GRATING"] = "MUSE"
                     self.header["FILTER"] = "MUSE"
 
@@ -2949,6 +2986,67 @@ class ReducedCube:
         info = f"{self.file} {self.shape}  {self.grating}_{self.filter}"
         info += f'  ({self.header["CRVAL1"]:.6f}, {self.header["CRVAL2"]:.6f})  z={self.redshift:.4f}'
         return info
+
+    def exposure_files(self, s3_prefix="s3://msaexp-nirspec/ifu_exposures", extensions=["ptab.fits", "satflag.fits.gz"], download_kwargs=None):
+        """
+        Get paths to individual exposures files that were used to make the cube
+
+        Parameters
+        ----------
+        s3_prefix : str
+            S3 object prefix
+
+        extensions : list
+            File types to retrieve.  For a given ``rate.fits`` exposure file,
+            the file objects are stored at
+            ``{s3_prefix}/jw{obsid}/jw{obsid}_[00000]_[00000]_nrs[1,2]_{ext}``
+
+            Available extensions are
+
+              - ``ptab.fits``: Full exposure pixel table (~90 MB)
+              - ``fs.fits``: Information extracted from the fixed-slit
+                 apertures that can be useful for estimating the sky
+                 background (~10 MB)
+              - ``satflag.fits.gz``: List of saturated pixels that can cause
+                 persistence (~50 KB)
+              - ``cal.yaml``: Full metadata of the pipeline ``cal``
+                product (~7 KB)
+              - ``rate_onef_axis0.png``: Diagnostic figure from the 1/f
+                 correction (~70 KB)
+
+        download_kwargs : None, dict
+            If provided (e.g., an empty dict) then retrieve the files,
+            otherwise, just generate the file lists.
+
+        Returns
+        -------
+        s3_files : dict
+            Dictionary with keys of the file ``extensions`` and values of
+            lists of S3 object paths for the individual files
+
+        """
+        from .cloud.utils import download_file
+
+        h_ = self.header
+
+        s3_files = {}
+
+        for i in range(h_['NFILES']):
+            file_prefix = h_[f"FILE{i:04d}"].split("_rate")[0]
+            for ext in extensions:
+                if i == 0:
+                    s3_files[ext] = []
+
+                s3_files[ext].append(
+                    f"{s3_prefix}/jw{h_['OBSID']}/{file_prefix}_{ext}".lower()
+                )
+
+        if download_kwargs is not None:
+            for ext in extensions:
+                for file in s3_files[ext]:
+                    resp = download_file(file, **download_kwargs)
+
+        return s3_files
 
     def load_sensitivity(self, **kwargs):
         """
@@ -3388,9 +3486,12 @@ class ReducedCube:
         optimize="loss",
         min_line_threshold=-4,
         broad_component_sigma=None,
+        optimizer="lstsq",
         **kwargs,
     ):
         """ """
+        from scipy.optimize import nnls
+
         from tqdm import tqdm
 
         if self.spec is None:
@@ -3521,6 +3622,10 @@ class ReducedCube:
             f"fit_emission_line: {lines} {scale_disp:.1f} σ={velocity_sigma:.0f} "
             f"z={self.redshift:.4f} nslice={nsl} dv={dv_ii:.1f} km/s  {nbins} bins  {ngrid} steps"
         )
+
+        if templates is not None:
+            msg += f"   N={len(templates)} templates"
+
         utils.log_comment(utils.LOGFILE, msg, verbose=VERBOSITY)
 
         A = np.vstack([c, np.zeros((nlines, nsl))])
@@ -3559,7 +3664,10 @@ class ReducedCube:
                 Ax = (A * swht_sl[:, j, i])[:, oki]
                 yxi = yx[:, j, i][oki]
 
-                lsq = np.linalg.lstsq(Ax.T, yxi, rcond=None)
+                if optimizer == "lstsq":
+                    lsq = np.linalg.lstsq(Ax.T, yxi, rcond=None)
+                else:
+                    lsq = nnls(Ax.T, yxi)
 
                 c_ji = lsq[0] * 1.0
 
@@ -3568,7 +3676,7 @@ class ReducedCube:
                         v_ji = utils.safe_invert(Ax.dot(Ax.T)).diagonal()
                         vcoeffs[k, :, j, i] = v_ji
                         clip_line = is_line & (
-                            c_ji < min_line_threshold * np.sqrt(v_ji)
+                            c_ji <= min_line_threshold * np.sqrt(v_ji)
                         )
                         c_ji[clip_line] = 0.0
                     except:
@@ -3618,14 +3726,18 @@ class ReducedCube:
             "max_line_dv": max_line_dv,
             "max_model": max_model,
             "get_covariance": get_covariance,
+            "msk": msk,
         }
 
         return ldata
 
     def log_sigma_grid(
-        self, min_dv_factor=0.5, max_sigma=500, step_factor=2, **kwargs
+        self, min_dv_factor=0.5, max_sigma=500, step_factor=2, values=None, **kwargs
     ):
         """ """
+        if values is not None:
+            return np.array(values)
+
         min_wave_dv = np.ceil(self.gradient_dv.min() * min_dv_factor / 25) * 25
 
         sigmas = np.exp(
@@ -4393,6 +4505,7 @@ class LinefitData:
         sn_threshold=4,
         discrete_sigma=True,
         show_contours=True,
+        mask_min_weight=0.1,
         **kwargs,
     ):
         """ """
@@ -4428,7 +4541,7 @@ class LinefitData:
         mean_image, mean_weight = self.cube.mean_image(
             wave_range=(self.cube.wave[imin], self.cube.wave[imax])
         )
-        weight_mask = mean_weight > 0.1 * np.nanmedian(mean_weight)
+        weight_mask = mean_weight > mask_min_weight * np.nanmedian(mean_weight)
         if weight_mask.sum() < 0.1 * weight_mask.size:
             weight_mask = np.isfinite(mean_weight)
 

msaexp/resample_numba.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numba import jit
-from math import erf, atan, pow as _pow, pi as math_pi
+from math import erf, atan, pow as _pow, pi as math_pi, sqrt
 
 import grizli.utils_numba.interp
 
@@ -145,12 +145,58 @@ def trapz(y, x):
 
 
 @jit(nopython=True, fastmath=True, error_model="numpy")
+def trapz_unc(y, x, unc):
+    """
+    Accelerated trapezoid rule integration with uncertainties on dependent
+    variable
+
+    Parameters
+    ----------
+    y : array-like
+        dependent variable
+
+    x : array-like
+        independent variable
+
+    unc : array-like
+        Uncertainty on independent variable
+
+    Returns
+    -------
+    result : float
+        Numerical integral of y(x)
+
+    unc_result : float
+        Propagated uncertainty on integral of y(x)
+
+    """
+    N = len(x)
+
+    h = (x[1] - x[0]) / 2.0
+    result = y[0] * h
+    unc_result = (h * unc[0])**2
+    h0 = h
+
+    for i in range(1, N - 1):
+        h1 = (x[i + 1] - x[i]) / 2.0
+        result += y[i] * (h0 + h1)
+        unc_result += ((h0+h1) * unc[i])**2
+        h0 = h1
+
+    result += y[-1] * h1
+    unc_result += (h1 * unc[-1])**2
+
+    return result, sqrt(unc_result)
+
+
+@jit(nopython=True, fastmath=True, error_model="numpy")
 def resample_template_numba(
     spec_wobs,
     spec_R_fwhm,
     templ_wobs,
     templ_flux,
-    templ_weight=None,
+    templ_unc=None,
+    spec_dwobs=None,
     velocity_sigma=100,
     nsig=5,
     fill_value=0.0,
@@ -177,6 +223,9 @@ def resample_template_numba(
 
     templ_flux : array-like
         Template flux densities sampled at `templ_wobs`
+
+    templ_unc : array-like
+        Optional uncertainties on ``templ_flux``
 
     velocity_sigma : float
         Kinematic velocity width, km/s
@@ -224,10 +273,10 @@ def resample_template_numba(
 
     Nt = len(templ_wobs)
 
-    has_weight = templ_weight is not None
+    has_unc = templ_unc is not None
 
-    if has_weight:
-        resamp_weight = np.ones_like(spec_wobs) * fill_value
+    if has_unc:
+        resamp_unc = np.ones_like(spec_wobs) * fill_value
 
     for i in range(N):
         if spec_wobs[i] < wave_min:
@@ -259,22 +308,34 @@ def resample_template_numba(
 
         sl = slice(ilo, ihi)
         lsl = templ_wobs[sl]
+        fsl = templ_flux[sl]
+        if has_unc:
+            usl = templ_unc[sl]
+
         g = np.exp(-((lsl - spec_wobs[i]) ** 2) / 2 / dw[i] ** 2) / np.sqrt(
             2 * np.pi * dw[i] ** 2
         )
-        # g *= 1./np.sqrt(2*np.pi*dw[i]**2)
-        if has_weight:
-            resamp_weight[i] = trapz(templ_weight[sl] * g, lsl)
 
-            resamp[i] = (
-                trapz(templ_flux[sl] * templ_weight[sl] * g, lsl)
-                / resamp_weight[i]
+        # Integrate across pixel
+        if spec_dwobs is not None:
+            in_wstep = np.abs(lsl - spec_wobs[i]) <= spec_dwobs[i] / 2.
+            lsl = lsl[in_wstep]
+            fsl = fsl[in_wstep]
+            g = g[in_wstep]
+
+            if has_unc:
+                usl = usl[in_wstep]
+
+        # g *= 1./np.sqrt(2*np.pi*dw[i]**2)
+        if has_unc:
+            resamp[i], resamp_unc[i] = trapz_unc(
+                fsl * g, lsl, usl * g
             )
         else:
-            resamp[i] = trapz(templ_flux[sl] * g, lsl)
+            resamp[i] = trapz(fsl * g, lsl)
 
-    if has_weight:
-        return np.append(resamp, resamp_weight)
+    if has_unc:
+        return np.append(resamp, resamp_unc)
     else:
         return resamp
 

msaexp/spectrum.py
@@ -283,6 +283,21 @@ class SpectrumSampler(object):
         """
         return self.spec.meta
 
+    @property
+    def label(self):
+        """
+        """
+        if "label" in self.spec.meta:
+            return self.spec.meta["label"]
+        elif self.file is not None:
+            return os.path.basename(self.file)
+        elif "SRCNAME" in self.spec.meta:
+            return self.spec.meta["SRCNAME"]
+        elif "GRATING" in self.spec.meta:
+            return "spec {GRATING}-{FILTER}".format(**self.spec.meta).lower()
+        else:
+            return ""
+
     def initialize_emission_line(self, nsamp=64):
         """
         Initialize emission line
@@ -417,11 +432,13 @@ class SpectrumSampler(object):
         """
 
         templ_wobs = template.wave.astype(np.float32) * (1 + z) / 1.0e4
-        # if fnu:
-        #     templ_flux = template.flux_fnu(z=z).astype(np.float32)
-        # else:
-        #     templ_flux = template.flux_flam(z=z).astype(np.float32)
         templ_flux = template.flux_fnu(z=z).astype(np.float32)
+
+        if hasattr(template, 'unc'):
+            templ_unc = template.unc
+            res_unc = np.zeros_like(self.spec_wobs)
+        else:
+            templ_unc = None
 
         if with_igm:
             igmz = IGM_FUNC(z, templ_wobs * 1.0e4)
@@ -439,21 +456,230 @@ class SpectrumSampler(object):
             elif self.sensitivity[order] is None:
                 continue
 
-            res_i = RESAMPLE_FUNC(
+            res_ = RESAMPLE_FUNC(
                 self.spec_wobs,
                 self.spec_R_fwhm * scale_disp * order,
                 templ_wobs * order,
                 templ_flux * igmz,
+                templ_unc=templ_unc,
                 velocity_sigma=velocity_sigma,
                 nsig=nsig,
             )
+
+            if templ_unc is None:
+                res_i = res_
+            else:
+                res_i, res_unc = res_.reshape(2, self.NSPEC)
 
             if order != 1:
                 res += res_i * self.sensitivity[order] * self.inv_sensitivity
             else:
                 res += res_i
 
-        return res * self.spec["to_flam"]**(fnu is False)
+        if templ_unc is None:
+            return res * self.spec["to_flam"]**(fnu is False)
+        else:
+            return (
+                res * self.spec["to_flam"]**(fnu is False),
+                res_unc * self.spec["to_flam"]**(fnu is False)
+            )
+
+    def smooth_velocity(self, velocity_sigma=500., unc="err", full_object=False, full_sliced=False, mask="valid", **kwargs):
+        """
+        Smooth spectrum in velocity space
+        """
+        import copy
+        import eazy.templates
+
+        if isinstance(mask, str):
+            if mask == "valid":
+                mask = self.spec["valid"] > 0
+            elif mask in self.spec.colnames:
+                mask = self.spec[mask] > 0
+        elif mask is None:
+            mask = np.isfinite(self.spec["flux"])
+
+        tsp = eazy.templates.Template(
+            arrays=(
+                self.spec["wave"][mask] * 1.e4,
+                (self.spec["flux"] * self.spec["to_flam"])[mask] * 1.e9
+            )
+        )
+
+        if isinstance(unc, str):
+            if unc == "valid":
+                tsp.unc = self.spec["valid"][mask] * 1
+            elif unc in self.spec.colnames:
+                tsp.unc = self.spec[unc][mask] * 1.0
+        elif unc is not None:
+            tsp.unc = unc[mask] * 1.0
+        else:
+            tsp.unc = None
+
+        result = self.resample_eazy_template(
+            tsp,
+            velocity_sigma=velocity_sigma,
+            **kwargs
+        )
+
+        if tsp.unc is None:
+            resamp, resamp_unc = result, result * 0.
+        else:
+            resamp, resamp_unc = result
+            bad = resamp_unc > self.spec["err"]
+            resamp_unc[bad] = 0.0
+            
+        if full_object:
+            wstep = np.exp(
+                np.arange(
+                    np.log(self.spec["wave"][mask].min()),
+                    np.log(self.spec["wave"][mask].max()),
+                    velocity_sigma / 2.5 / 3.e5)
+                )
+
+            if full_sliced:
+                wi = np.round(
+                    np.interp(
+                        wstep[3:-3], self.spec["wave"], np.arange(self.NSPEC))
+                ).astype(int)
+            else:
+                wi = np.arange(self.NSPEC, dtype=int)
+
+            spc = copy.deepcopy(self)
+            spc.NSPEC = len(wi)
+            spc.spec = self.spec[wi]
+            spc.spec["wave_index"] = wi
+            spc.spec_wobs = (spc.spec["wave"] * 1.0).astype(np.float32)
+            
+            Rdisp = 1.0 / spc.spec["R"]**2 + (2.35 * velocity_sigma / 3e5)**2
+            spc.spec_R_fwhm = (1.0 / np.sqrt(Rdisp)).astype(np.float32)
+
+            spc.spec["flux"] = resamp[wi]
+            spc.spec["err"] = resamp_unc[wi]
+            spc.spec["valid"] = (spc.spec["flux"] != 0) & (resamp_unc[wi] > 0)
+            spc.meta["smooth_velocity"] = velocity_sigma
+
+            for order in spc.sensitivity:
+                if spc.sensitivity[order] is not None:
+                    if hasattr(spc.sensitivity[order], "__len__"):
+                        spc.sensitivity[order] = spc.sensitivity[order][wi]
+
+            if isinstance(self.spec_input, pyfits.HDUList):
+
+                orig_cols = [
+                    c.name for c in spc.spec_input["SPEC1D"].columns
+                ]
+
+                spc.spec_input["SPEC1D"] = pyfits.BinTableHDU(
+                    spc.spec[orig_cols]
+                )
+
+                if "WHT" in self.spec_input:
+                    err2d = 1.0 / np.sqrt(self.spec_input["WHT"].data)
+                    NY = err2d.shape[0]
+
+                    spc.spec_input["WHT"].data = (
+                        self.spec_input["WHT"].data[:, wi]
+                    )
+
+                    for ext in ["SCI", "PROFILE", "BACKGROUND"]:
+                        if ext not in self.spec_input:
+                            continue
+
+                        spc.spec_input[ext].data = (
+                            self.spec_input[ext].data[:, wi]
+                        )
+
+                        for i, row in enumerate(self.spec_input[ext].data):
+                            row_i = (row * self.spec["to_flam"])[mask] * 1.e9
+
+                            tsp.flux[0,:] = row_i
+                            tsp.unc = err2d[i,:] * 1.0
+
+                            result = self.resample_eazy_template(
+                                tsp,
+                                velocity_sigma=velocity_sigma,
+                                **kwargs
+                            )
+
+                            resamp, resamp_unc = result
+                            spc.spec_input[ext].data[i,:] = resamp[wi]
+
+                            if ext == "SCI":
+                                spc.spec_input["WHT"].data[i,:] = (
+                                    1.0 / resamp_unc[wi]**2
+                                )
+
+            return spc
+
+        else:
+            return resamp, resamp_unc
+
+
+    def regrid(self, wstep=None, velocity_sigma=250, mask="valid"):
+        """
+        """
+        from .resample_numba import trapz_unc
+        
+        if isinstance(mask, str):
+            if mask == "valid":
+                mask = self.spec["valid"] > 0
+            elif mask in self.spec.colnames:
+                mask = self.spec[mask] > 0
+        elif mask is None:
+            mask = np.isfinite(self.spec["flux"])
+
+        if wstep is None:
+            wstep = np.exp(
+                np.arange(
+                    np.log(self.spec["wave"].min()),
+                    np.log(self.spec["wave"].max()),
+                    velocity_sigma / 3.e5)
+                )
+
+        wgrid = msautils.array_to_bin_edges(wstep)
+        new = utils.GTable()
+
+        new["wave"] = wstep
+        new["flux"] = 0.0
+        new["err"] = 0.0
+
+        new["R"] = np.interp(
+            wstep, self.spec["wave"], self.spec["R"],
+            left=self.spec["R"][0], right=self.spec["R"][-1]
+        )
+        new["nbin"] = 0
+
+        N = len(wstep)
+
+        nu = 1.0 / self.spec["wave"]
+
+        for i in range(N):
+            wbin = (
+                (self.spec["wave"] >= wgrid[i])
+                & (self.spec["wave"] < wgrid[i+1])
+                & self.spec["valid"] & mask
+            )
+            nbin = wbin.sum()
+            if nbin > 1:
+                nu_bin = nu[wbin][::-1]
+                dnu = nu_bin[-1] - nu_bin[0]
+
+                resamp, resamp_unc = trapz_unc(
+                    self.spec["flux"][wbin][::-1],
+                    nu_bin,
+                    self.spec["err"][wbin][::-1],
+                )
+
+                new["flux"][i] = resamp / dnu
+                new["err"][i] = resamp_unc / dnu
+                new["nbin"][i] = wbin.sum()
+
+        for k in self.spec.meta:
+            new.meta[k] = self.spec.meta[k]
+
+        return SpectrumSampler(new)
+
 
     def emission_line(
         self,
@@ -874,9 +1100,15 @@ class SpectrumSampler(object):
         """
         if isinstance(self.spec_input, pyfits.HDUList):
             fig = msautils.drizzled_hdu_figure(self.spec_input, **kwargs)
-        else:
+        elif self.file is None:
             with pyfits.open(self.file) as hdul:
                 fig = msautils.drizzled_hdu_figure(hdul, **kwargs)
+        else:
+            hdul = pyfits.HDUList([
+                pyfits.PrimaryHDU(),
+                pyfits.BinTableHDU(self.spec, name='SPEC1D')
+            ])
+            fig = msautils.drizzled_hdu_figure(hdul, **kwargs)
 
         return fig
 
@@ -1176,7 +1408,7 @@ class SpectrumSampler(object):
 
         valid &= ~low_sensitivity
 
-        msg = f"{os.path.basename(self.file)}  z={z:.3f}  "
+        msg = f"{self.label}  z={z:.3f}  "
         msg += f"{w[valid].min():.2f} – {w[valid].max():.2f} µm"
         utils.log_comment(utils.LOGFILE, msg, verbose=verbose)
 
@@ -1264,10 +1496,13 @@ GRATING_COLORS = {
     "PRISM": "purple",
     "G395M": "tomato",
     "G235M": "goldenrod",
-    "G140M": "steelblue"
+    "G140M": "steelblue",
+    "G395H": "coral",
+    "G235H": "gold",
+    "G140H": "lightblue",
 }
 
-def multiplot_spectra(file="smacs0723-ero-v4_g395m-f290lp_2736_6355.spec.fits", ny=8, log_steps=True, wave_limits=None, renorm=True, extra="", spline_step=64, gratings=[], trim_empty_axes=True, grating_colors=GRATING_COLORS, **kwargs):
+def multiplot_spectra(file="smacs0723-ero-v4_g395m-f290lp_2736_6355.spec.fits", ny=8, z=None, log_steps=True, wave_limits=None, renorm=True, extra="", spline_step=64, gratings=[], trim_empty_axes=True, grating_colors=GRATING_COLORS, **kwargs):
     """
     Query and plot overlapping spectra
 
@@ -1314,9 +1549,10 @@ def multiplot_spectra(file="smacs0723-ero-v4_g395m-f290lp_2736_6355.spec.fits", 
     import eazy.filters
 
     query_url = (
-        "https://grizli-cutout.herokuapp.com/nirspec_extractions?file=" + file + "&output=csv" + extra
+        "https://grizli-cutout.herokuapp.com/nirspec_extractions?file="
+        + file + "&output=csv" + extra
     )
-    print(query_url)
+    print(query_url),
 
     spec_list = utils.read_catalog(query_url, format="csv")
 
@@ -1326,7 +1562,8 @@ def multiplot_spectra(file="smacs0723-ero-v4_g395m-f290lp_2736_6355.spec.fits", 
     else:
         keep = np.ones(len(spec_list), dtype=bool)
 
-    z = spec_list["z"][(spec_list["grade"] > 2.5) & (keep)].mean()
+    if z is None:
+        z = spec_list["z"][(spec_list["grade"] > 2.5) & (keep)].mean()
 
     for i, row in enumerate(spec_list):
         print(
@@ -2893,27 +3130,38 @@ def read_spectrum(
             # grating_degree=2 default poly fit for gratings
             **kwargs
         )
+
         spec["R"] = R_fwhm
         spec["R"].description = "Spectral resolution from tabulated curves"
+
+        if "smooth_velocity" in spec.meta:
+            vsm = spec.meta["smooth_velocity"]
+            Rdisp = 1.0 / spec["R"]**2 + (2.35 * vsm / 3.e5)**2
+            spec["R"] = 1.0 / np.sqrt(Rdisp)
+            spec["R"].description += f" + smooth_velocity={vsm:.1f} km/s"
 
     spec.grating = grating
     spec.filter = _filter
 
     flam_unit = 1.0e-20 * u.erg / u.second / u.cm**2 / u.Angstrom
 
-    um = spec["wave"].unit
-    if um is None:
-        um = u.micron
+    wave_unit = spec["wave"].unit
+    if spec["wave"].unit is None:
+        wave_unit = u.micron
 
-    spec.equiv = u.spectral_density(spec["wave"].data * um)
+    flux_unit = spec["flux"].unit
+    if flux_unit is None:
+        flux_unit = u.microJansky
+
+    spec.equiv = u.spectral_density(spec["wave"].data * wave_unit)
 
     spec["to_flam"] = (
-        (1 * spec["flux"].unit).to(flam_unit, equivalencies=spec.equiv).value
+        (1 * flux_unit).to(flam_unit, equivalencies=spec.equiv).value
     )
     spec.meta["flamunit"] = flam_unit.unit
 
-    spec.meta["fluxunit"] = spec["flux"].unit
-    spec.meta["waveunit"] = spec["wave"].unit
+    spec.meta["fluxunit"] = flux_unit
+    spec.meta["waveunit"] = wave_unit
 
     spec["wave"] = spec["wave"].value
     spec["flux"] = spec["flux"].value